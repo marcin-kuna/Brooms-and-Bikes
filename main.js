6b8ddea7b7ed86541b83de7d59757f4c4e925d97
Marcin Kuna <marcin.m.kuna@gmail.com>
2019-05-21 23:46:26 +0200

    Added smooth scroll

--- main.js
+++ main.js
@@ -7,6 +7,46 @@ navBarToggle.addEventListener('click', () =>
     mainNav.classList.toggle('active')
 );
 
+// SMOOTH SCROLL
+
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarLinks.forEach(elem => elem.addEventListener('click', navbarLinkClick));
+
+function navbarLinkClick(event){
+    smoothScroll(event);
+}
+
+function smoothScroll(event){
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href") === "#" ? "header" : event.currentTarget.getAttribute("href");
+    const targetPosition = document.querySelector(targetId).offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1000;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp){
+        if(!start) start = timestamp;
+        const progress = timestamp - start;
+        // window.scrollTo(0, distance*(progress/duration) + startPosition);    //linear
+        window.scrollTo(0, easeInOutQuad(progress, startPosition, distance, duration));
+        if(progress < duration) window.requestAnimationFrame(step);
+    }
+}
+
+// EASING FOR SMOOTH SCROLL
+
+function easeInOutQuad(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+
+
 // RPG STAR
 
 const starInfo = document.getElementById('star-info');

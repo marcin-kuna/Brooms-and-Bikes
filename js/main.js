d76f85b5a64748b7dddfabcbab5bb8d68e1fe377
Marcin Kuna <marcin.m.kuna@gmail.com>
2019-07-19 23:17:33 +0200

    Reworked Footer Section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// FOOTER COUNTER
+
+function animateValue(id, start, end, duration) {
+  let range = end - start;
+  let current = start;
+  let increment = end > start ? 1 : -1;
+  let stepTime = Math.abs(Math.floor(duration / range));
+  let obj = document.getElementById(id);
+  let timer = setInterval(function() {
+      current += increment;
+      obj.innerHTML = current;
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
+}
+
+animateValue("number-one", 0, 20, 1200);
+animateValue("number-two", 0, 99, 1200);
+animateValue("number-three", 0, 450, 1200);
+
+
 // FULLPAGE SCROLL
 
 let slideElems = document.querySelectorAll('.slide-section');
